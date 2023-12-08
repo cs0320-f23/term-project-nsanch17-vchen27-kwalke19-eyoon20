e58e14ef224967f2cbd65f904cd5a804c78ee344
vickyczx <vicky_chen1@brown.edu>
2023-12-08 12:23:39 -0500

    refactor: changed query parameter names.

--- backend/posting.py
+++ backend/posting.py
@@ -3,19 +3,19 @@ from flask import Flask, request, jsonify
 class PostingHandler:
     @staticmethod
     def handle_posting():
-        name = request.args.get("name")
+        item_name = request.args.get("item_name")
         price = request.args.get("price")
         description = request.args.get("description")
-        date_posted = request.args.get("date_posted")
+        date = request.args.get("date")
 
         if not name or not price or not description or not date_posted:
             return jsonify({"error": "Missing parameters"}), 400
 
         item_data = {
-            "name": name,
+            "item_name": item_name,
             "price": price,
             "description": description,
-            "date_posted": date_posted
+            "date": date
         }
 
         return jsonify({"item": item_data})
