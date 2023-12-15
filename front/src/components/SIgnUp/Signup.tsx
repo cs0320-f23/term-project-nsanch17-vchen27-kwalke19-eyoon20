ae0a5a34d2751af150a45ad7ad49ab51706c9a84
nicolesanchez-soto <nicole_sanchez-soto@brown.edu>
2023-12-15 18:01:31 -0500

    feat: created login page

--- front/src/components/SIgnUp/Signup.tsx
+++ front/src/components/SIgnUp/Signup.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import "../../style/Signup.css"; // Importing the CSS file for styling
+import "../../style/Signup.css";
 
 const Signup: React.FC = () => {
+  const [isSignup, setIsSignup] = useState(true); // State to toggle between signup and login
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [firstName, setFirstName] = useState("");
@@ -11,42 +12,57 @@ const Signup: React.FC = () => {
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    // Handle the signup logic here
-    console.log({ firstName, lastName, email, password, number, username });
+    if (isSignup) {
+      // Handle the signup logic here
+      console.log({ firstName, lastName, email, password, number, username });
+    } else {
+      // Handle the login logic here
+      console.log({ email, password });
+    }
+  };
+
+  const toggleForm = () => {
+    setIsSignup(!isSignup);
   };
 
   return (
     <div className="signup-container">
       <form onSubmit={handleSubmit} className="signup-form">
-        <h2 className="form-title">Sign Up</h2>
-        <input
-          type="text"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-          placeholder="First Name"
-          className="form-input"
-        />
-        <input
-          type="text"
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-          placeholder="Last Name"
-          className="form-input"
-        />
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder="Username"
-          className="form-input"
-        />
-        <input
-          type="tel"
-          value={number}
-          onChange={(e) => setNumber(e.target.value)}
-          placeholder="Phone Number"
-          className="form-input"
-        />
+        <h2 className="form-title">{isSignup ? "Sign Up" : "Login"}</h2>
+
+        {isSignup && (
+          <>
+            <input
+              type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              placeholder="First Name"
+              className="form-input"
+            />
+            <input
+              type="text"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              placeholder="Last Name"
+              className="form-input"
+            />
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Username"
+              className="form-input"
+            />
+            <input
+              type="tel"
+              value={number}
+              onChange={(e) => setNumber(e.target.value)}
+              placeholder="Phone Number"
+              className="form-input"
+            />
+          </>
+        )}
+
         <input
           type="email"
           value={email}
@@ -61,8 +77,17 @@ const Signup: React.FC = () => {
           placeholder="Password"
           className="form-input"
         />
+
         <button type="submit" className="signup-button">
-          Sign Up
+          {isSignup ? "Sign Up" : "Login"}
+        </button>
+
+        <button
+          type="button"
+          onClick={toggleForm}
+          className="toggle-form-button"
+        >
+          {isSignup ? "Or Login" : "Or Sign Up"}
         </button>
       </form>
     </div>
