7664e6be12998a1387463400f7e0e95f7abcb8e7
nicolesanchez-soto <nicole_sanchez-soto@brown.edu>
2023-12-15 17:57:17 -0500

    feat: created signup page (added more inputs)

--- front/src/components/SIgnUp/Signup.tsx
+++ front/src/components/SIgnUp/Signup.tsx
@@ -4,17 +4,49 @@ import "../../style/Signup.css"; // Importing the CSS file for styling
 const Signup: React.FC = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [number, setNumber] = useState("");
+  const [username, setUsername] = useState("");
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     // Handle the signup logic here
-    console.log(email, password);
+    console.log({ firstName, lastName, email, password, number, username });
   };
 
   return (
     <div className="signup-container">
       <form onSubmit={handleSubmit} className="signup-form">
         <h2 className="form-title">Sign Up</h2>
+        <input
+          type="text"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          placeholder="First Name"
+          className="form-input"
+        />
+        <input
+          type="text"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          placeholder="Last Name"
+          className="form-input"
+        />
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
+          className="form-input"
+        />
+        <input
+          type="tel"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+          placeholder="Phone Number"
+          className="form-input"
+        />
         <input
           type="email"
           value={email}
