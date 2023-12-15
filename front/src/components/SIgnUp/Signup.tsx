1308a7549838ca860344a1ba012781b0e6faa24c
nicolesanchez-soto <nicole_sanchez-soto@brown.edu>
2023-12-15 18:16:35 -0500

    feat: added email authentication logic

--- front/src/components/SIgnUp/Signup.tsx
+++ front/src/components/SIgnUp/Signup.tsx
@@ -2,34 +2,53 @@ import React, { useState } from "react";
 import "../../style/Signup.css";
 
 const Signup: React.FC = () => {
-  const [isSignup, setIsSignup] = useState(true); // State to toggle between signup and login
+  const [isSignup, setIsSignup] = useState(true);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [number, setNumber] = useState("");
   const [username, setUsername] = useState("");
+  const [error, setError] = useState(""); // State to store the error message
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    setError(""); // Clear any existing errors
+
+    // Check if the email domain is brown.edu
+    const emailDomain = email.split("@")[1];
+    if (emailDomain !== "brown.edu") {
+      setError("You must use a brown.edu email address to sign up.");
+      return;
+    }
+
     if (isSignup) {
-      // Handle the signup logic here
-      console.log({ firstName, lastName, email, password, number, username });
+      // Handle the signup logic here, interact with post from backend.
+      console.log("Signup successful:", {
+        firstName,
+        lastName,
+        email,
+        password,
+        number,
+        username,
+      });
     } else {
-      // Handle the login logic here
-      console.log({ email, password });
+      // Handle the login logic here, interact with get from backend.
+      console.log("Login successful:", { email, password });
     }
   };
 
   const toggleForm = () => {
     setIsSignup(!isSignup);
+    setError(""); // Clear errors when toggling between forms
   };
 
   return (
     <div className="signup-container">
       <form onSubmit={handleSubmit} className="signup-form">
         <h2 className="form-title">{isSignup ? "Sign Up" : "Login"}</h2>
-
+        {error && <div className="form-error">{error}</div>}{" "}
+        {/* Display any error messages here */}
         {isSignup && (
           <>
             <input
@@ -62,7 +81,6 @@ const Signup: React.FC = () => {
             />
           </>
         )}
-
         <input
           type="email"
           value={email}
@@ -77,11 +95,9 @@ const Signup: React.FC = () => {
           placeholder="Password"
           className="form-input"
         />
-
         <button type="submit" className="signup-button">
           {isSignup ? "Sign Up" : "Login"}
         </button>
-
         <button
           type="button"
           onClick={toggleForm}
